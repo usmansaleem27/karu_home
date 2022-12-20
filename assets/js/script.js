b12000942b2fc432b1f253a3ff89420759d457ef
usmansaleem27 <bc220409199usa@vu.edu.pk>
2022-12-20 16:57:29 +0500

    Changes in Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-function navbarToggle(x) {
+function navbarToggle() {
     document.getElementById("myDropdown").classList.toggle("show");
+    var audio = document.getElementById("audio");
+     audio.play();
+    
   }
 
 
