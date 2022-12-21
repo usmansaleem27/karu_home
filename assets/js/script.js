00c2ef092c2013544155714e4856a9ee3a48085d
usmansaleem27 <bc220409199usa@vu.edu.pk>
2022-12-21 17:53:19 +0500

    Changes in Website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
+
 function navbarToggle() {
     document.getElementById("myDropdown").classList.toggle("show");
     var audio = document.getElementById("audio");
      audio.play();
-    
   }
 
   var icon = document.getElementById("icon");
   icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+      icon.classList = "bi bi-moon-stars-fill fs-3"
+    }
+    else(
+      icon.classList = "bi bi-brightness-high-fill fs-3"
+    )
   }
 
 
